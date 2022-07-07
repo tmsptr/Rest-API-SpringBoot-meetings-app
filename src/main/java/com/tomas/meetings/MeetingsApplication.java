a37bb10c6460c8b011b96f3d7c02402df3e50dba
Tomas Petruoka <petruokat@gmail.com>
2022-07-07 15:46:00 +0300

    fixes

---
+++ src/main/java/com/tomas/meetings/MeetingsApplication.java
@@ -0,0 +1,13 @@
+package com.tomas.meetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomas/meetings/MeetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomas.meetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
